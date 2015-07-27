ebbc7fb1838a1d6705d13546e5db68ee3a746e92
jonathan.haywood@gmail.com <jonathan.haywood@gmail.com>
2015-07-27 17:46:03 -0600

    Moved some hard coded settings into config file.

--- source/app.js
+++ source/app.js
@@ -1,23 +1,25 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var config = require('./config');
 var apiRouter = require('./routes/apiRoutes');
 var webRouter = require('./routes/webRoutes');
 
-var db = mongoose.connect('mongodb://localhost/realMomsAPI');
+var db = mongoose.connect(config.databaseConnectionString);
 var app = express();
-var port = process.env.PORT || 8000;
+var port = process.env.PORT || config.defaultPort;
 
-app.set('views', './views');
+app.set('views', './views'); // etll express where our views are
 app.set('view engine', 'vash'); // use vash as view engine
 
 app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
 app.use(express.static('public')); // set up static files
-app.use('/api', apiRouter()); // use api router for api routes
+app.use('/api', apiRouter()); // use api router for ajax api routes
 
 app.get('/', webRouter.index);
 
+// set the server
 app.listen(port, function () {
 	console.log('Running on port: ' + port);
 });
--- source/config.js
+++ source/config.js
@@ -1,5 +1,10 @@
 var config = {};
 
+// database settings
+config.databaseConnectionString = 'mongodb://localhost/realMomsAPI';
+
+// application settings
+config.defaultPort = 8000;
 config.ratePerChild = 2;
 config.maxPerFamily = 5;
 
