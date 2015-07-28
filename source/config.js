e800532dae9993f162e4cb9f9e0171c478cadacb
jonathan.haywood@gmail.com <jonathan.haywood@gmail.com>
2015-07-28 12:15:28 -0600

    Setting ports and env.

--- source/config.js
+++ source/config.js
@@ -4,7 +4,8 @@ var config = {};
 config.databaseConnectionString = process.env.DB || 'mongodb://localhost/realMomsAPI';
 
 // application settings
-config.defaultPort = 8000;
+config.env = process.env.NODE_ENV || 'development';
+config.port = process.env.PORT || (process.env.NODE_ENV === 'production' ? 80 : 8000);
 config.ratePerChild = 2;
 config.maxPerFamily = 5;
 
